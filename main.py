ccba2130ba84382b8bc8d9c6f6f06f6586987379
arjunanan6 <arjun.anand1@gmail.com>
2022-11-29 19:31:17 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional, Any
+from random import randrange
+
+app = FastAPI()
+
+#Pydantic Basemodel: https://pydantic-docs.helpmanual.io/usage/models/
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True # Optional field where default value is true for post published.
+    rating: Optional[int] = None
+
+my_posts: dict[str | Any] = [{"title": "Title of post 1", "content": "Post 1", "id": 1},
+ {"title": "Title of post 2", "content": "Post 2", "id": 2}]
+
+def find_post(id: int):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
+        # Do not use else statement here as it fails to iterate over rows.
+
+@app.get("/")
+def root():
+    return {"message": "Hello testing"}
+
+
+@app.get("/posts")
+def get_posts():
+    return {"data": my_posts}
+
+# post in the following is a model inherited from the Post class where our type validation is done.
+@app.post("/posts")
+def create_posts(post: Post):
+    # Doing this because we are not using a transactional database. 
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0, 10000000000)
+    my_posts.append(post_dict)  #Every pydantic model can be converted to a dictionary if needed with dict()
+    return {"data": post_dict}
+
+@app.get("/posts/{id}")
+
+def get_post(id: int):
+
+    post = find_post(id) # Don't forget to convert to int! 
+    print(post)
+    return {"post_detail": post}
