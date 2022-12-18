33a4f8ea03461f61e2ed021ec00f2a1644434d36
Arjun <arjun@arjunanand.me>
2022-12-18 14:45:44 +0100

    Cleanup

--- main.py
+++ main.py
@@ -29,8 +29,6 @@ class Post(BaseModel):
 
 @app.get("/posts")
 def get_posts(db: Session = Depends(get_db)):
-    # cursor.execute("""SELECT * FROM POSTS """)
-    # posts = cursor.fetchall()
 
     posts = db.query(models.Post).all()
     return {"data": posts}
@@ -42,12 +40,6 @@ def get_posts(db: Session = Depends(get_db)):
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post, db: Session = Depends(get_db)):
-    # cursor.execute(
-    #     """INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """,
-    #     (post.title, post.content, post.published),
-    # )
-    # new_post = cursor.fetchone()
-    # conn.commit()
 
     new_post = models.Post(
         **post.dict()
@@ -73,8 +65,7 @@ def get_latest_post():
 
 @app.get("/posts/{id}")
 def get_post(id: int, response: Response, db: Session = Depends(get_db)):
-    # cursor.execute("""SELECT * FROM posts WHERE id = %s""", (str(id)))
-    # post = cursor.fetchone()
+
     post = db.query(models.Post).filter(models.Post.id == id).first()
     if not post:
         raise HTTPException(
@@ -89,9 +80,7 @@ def get_post(id: int, response: Response, db: Session = Depends(get_db)):
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(get_db)):
-    # cursor.execute("""DELETE FROM posts where id = %s RETURNING *""", (str(id)))
-    # deleted_post = cursor.fetchone()
-    # conn.commit()
+
     post = db.query(models.Post).filter(models.Post.id == id)
     if not post.first():
         raise HTTPException(
@@ -108,12 +97,7 @@ def delete_post(id: int, db: Session = Depends(get_db)):
 
 @app.put("/posts/{id}")
 def update_post(id: int, post: Post, db: Session = Depends(get_db)):
-    # cursor.execute(
-    #     """UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""",
-    #     (post.title, post.content, post.published, id),
-    # )
-    # updated_post = cursor.fetchone()
-    # conn.commit()
+
     post_query = db.query(models.Post).filter(models.Post.id == id)
     post_data = post_query.first()
     if not post_data:
